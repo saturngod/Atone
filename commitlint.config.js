5ab6969aca42a92d6d23f6431db690a1c7596de3
saturngod <saturngod@gmail.com>
2026-01-01 13:16:45 -0600

    feat: support lint commit message

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'chore',
+                'revert',
+            ],
+        ],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
+        'subject-case': [0], // Allow any case for subject
+    },
+};
--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ export default [
         },
     },
     {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js'],
+        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'commitlint.config.js'],
     },
     prettier, // Turn off all rules that might conflict with Prettier
 ];
